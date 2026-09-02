627b7c463a9811e6b058af26224c2fed301dbea6
usaaa1286-create <usaaa1286@gmail.com>
2026-09-02 00:16:00 +0000

    Fix missing getValue import

--- app/src/main/java/com/rustskinanalyzer/ui/trend/TrendScreen.kt
+++ app/src/main/java/com/rustskinanalyzer/ui/trend/TrendScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
